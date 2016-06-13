48abd4a388c4705d3aba3f774d8dc547c34f1a35
jwittenbach <jason.wittenbach@gmail.com>
2016-06-13 16:57:14 -0400

    adds security group configuration to setup

--- lodestone.py
+++ lodestone.py
@@ -16,13 +16,19 @@ def cli():
 @cli.command()
 @click.argument('cluster')
 def setup(cluster):
-    click.echo('setting up Thunder on ' + cluster + '...')
+    click.echo('setting up Thunder on ' + cluster + '...\n')
+
+    # set up security group to allow traffic for Jupyter notebook
+    click.echo('configuring security group...\n')
+    configure_sg()
     
     # clone the lodestone repo which contains scripts for future operations
+    click.echo('downloading lodestone scripts to cluster nodes...\n')
     run_all("sudo yum -y install git", cluster)
     run_all("git clone https://github.com/jwittenbach/lodestone", cluster)
 
     # run the script for installation on all nodes
+    click.echo('running setup scripts on cluster nodes...\n')
     run_all("bash lodestone/script_all.sh", cluster) 
 
 @cli.command()
@@ -30,7 +36,7 @@ def setup(cluster):
 def reboot(cluster):
     run_all('rm -rf lodestone miniconda*', cluster) 
 
-def sg():
+def configure_sg():
     # get Flintrock security groups and make sure ports for Jupyter notebook are open
     ec2 = boto3.resource(service_name='ec2')
     for sg in ec2.security_groups.filter(Filters=[{'Name': 'group-name', 'Values': ['flintrock']}]).all():
