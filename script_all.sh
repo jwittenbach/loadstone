02b72b4ff30cb38270a21e67020a2658b5415496
jwittenbach <jason.wittenbach@gmail.com>
2016-06-13 14:35:11 -0400

    adds miniconda to path

--- script_all.sh
+++ script_all.sh
@@ -4,8 +4,9 @@ sudo yum install -y gcc
 # install miniconda and required packages
 wget http://repo.continuum.io/miniconda/Miniconda-3.7.3-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
+export PATH=$HOME/miniconda/bin:$PATH
 conda update -y conda
 conda install -y pip numpy scipy scikit-learn scikit-image matplotlib
 
 # install thunder and related packages
-pip install thunder
+pip install thunder-python
