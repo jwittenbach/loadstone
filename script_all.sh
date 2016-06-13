026998bedd81b05af7080217e2f662daf91ac02e
jwittenbach <jason.wittenbach@gmail.com>
2016-06-13 16:12:27 -0400

    installs scikit-image from conda to avoid build that fails

--- script_all.sh
+++ script_all.sh
@@ -1,12 +1,12 @@
 # install C compiler
 sudo yum install -y gcc
 
-# install miniconda and required packages
+# install miniconda
 wget http://repo.continuum.io/miniconda/Miniconda-3.7.3-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH=$HOME/miniconda/bin:$PATH
 conda update -y conda
-conda install -y pip numpy scipy scikit-learn scikit-image matplotlib
 
 # install thunder and related packages
+conda install jupyter scikit-image
 pip install thunder-python
